b86b39468beb4b598f53d85efaddecc08bd90ab1
alinmateas <alin_10messi@yahoo.com>
2020-04-23 19:59:07 +0300

    init commit

---
+++ agendaPersonala/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace agendaPersonala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
